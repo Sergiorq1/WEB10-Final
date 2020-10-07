bbdbd8e45251bc864d5f838bd86eaa48d1f159f7
Sergiorq1 <66581064+Sergiorq1@users.noreply.github.com>
2020-10-06 17:11:55 -0700

    Update main.js

--- main.js
+++ main.js
@@ -6,3 +6,16 @@ const inputSize = document.querySelector('#input-size')
 // The element that displays the value 
 const showSize = document.querySelector('#show-size')
 // Repeat this for the input and "show" elements
+
+
+inputSize.addEventListener('input', handleInput)
+
+function handleInput() {
+	// get the font size:
+	const fontSize = inputSize.value + 'px'
+	// Set the style 
+	display.style.fontSize = fontSize
+	// Show the size
+	showSize.innerHTML = fontSize
+}
+
