59ef2e09ed51c50a5a947b773ce55562d0ae2a29
Sergiorq1 <66581064+Sergiorq1@users.noreply.github.com>
2020-10-06 17:09:18 -0700

    finish CSS/ start JS

---
+++ main.js
@@ -0,0 +1,8 @@
+// The headline element
+const display = document.querySelector('#display')
+
+// The input field for size
+const inputSize = document.querySelector('#input-size')
+// The element that displays the value 
+const showSize = document.querySelector('#show-size')
+// Repeat this for the input and "show" elements
